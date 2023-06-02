beba83cb82b6e2ee81eb9dc53b423ff8d0c76180
Mohammad Ali Parvez <mohammadaparvez@outlook.com>
2023-06-02 11:58:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool stillMore = false;
+            List<Student> students = new List<Student>();
+            do
+            {
+                Console.Write("Enter your first name, please: ");
+                string firstName = Console.ReadLine();
+                Console.Write("Enter your family name, please: ");
+                string familyName = Console.ReadLine();
+                Console.Write("Enter the Registration date of your studies, please: ");
+                DateTime sdor;
+                string nextdor = Console.ReadLine();
+
+                while (!DateTime.TryParse(nextdor, out sdor))
+                {
+                    Console.Write("Not valid, Try again ");
+                    nextdor = Console.ReadLine();
+
+                }
+                students.Add(new Student(firstName, familyName, sdor));
+                Console.Write("More (Y/N)?:");
+
+                
+                string moreOfThis = Console.ReadLine().ToUpper();
+                if (moreOfThis.StartsWith("Y"))
+                stillMore = true;
+
+                else
+                    stillMore=false;
+                
+                }while (!stillMore);
+
+            foreach (Student item in students)
+                Console.WriteLine(item);
+            
+
+
+                
+            
+            
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal class Student
+    {
+        //Constructor
+        public Student(string firstName, string familyName, DateTime registrationDate)
+        {
+            FirstName = firstName;
+            FamilyName= familyName;
+            RegistrationDate = registrationDate;
+            
+        }
+
+        //Properties
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        //Method
+
+        public override string ToString()
+        {
+            return "First name is " + FirstName + 
+                ", Family name is " + FamilyName +
+                ",\n Registration date was " + RegistrationDate.ToString("dddd,d,MMMM,yyyy");
+        }
+        
+
+        }
+
+
+    }
+
